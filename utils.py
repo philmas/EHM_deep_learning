2a75c1a5ec3adbe91cf012dfc25821e1ed62ae28
Philippe van Mastrigt <p.g.vanmastrigt@student.tudelft.nl>
2021-06-07 16:29:17 +0200

    First commit

--- utils.py
+++
@@ -1,8 +0,0 @@
-# TODO SUPPORT PNG
-def getImageUrls(folder):
-    # if path.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
-    return glob.glob(os.path.join(folder, '*.jpg'))
-
-
-def getD2Urls(folder):
-    return glob.glob(os.path.join(folder, '*.d2-net'))
